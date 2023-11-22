2aacf9a41aa1857151130116531101662a5ff2c6
usnimoj <jominsu0103@naver.com>
2023-11-22 15:15:07 +0900

    datasource 중괄호 이슈해결

--- src/models/userDao.js
+++ src/models/userDao.js
@@ -1,18 +1,18 @@
-const { appDataSource } = require('../utils/database')
-
+const appDataSource = require("../utils/database");
 
 const getUserByEmail = async (email) => {
-    const result = await appDataSource.query(
+  const result = await appDataSource.query(
     `SELECT * FROM users 
     WHERE 
     email = ?`,
-    [email]);
-    return result;
-  };
+    [email]
+  );
+  return result;
+};
 
 const createAdmin = async (email, password, kakao_id, name, is_admin) => {
-    const result = await appDataSource.query(
-      `INSERT INTO users
+  const result = await appDataSource.query(
+    `INSERT INTO users
     (
         email, 
         password,
@@ -22,32 +22,33 @@ const createAdmin = async (email, password, kakao_id, name, is_admin) => {
     )
     VALUES 
     (?,?,?,?,?)`,
-    [email, password, kakao_id, name, is_admin]);
-    return result;
+    [email, password, kakao_id, name, is_admin]
+  );
+  return result;
 };
 
 const createUser = async (kakaoId, name, email, birthday, phone_number) => {
-    try {
-      await appDataSource.query(
-        `INSERT INTO users(
+  try {
+    await appDataSource.query(
+      `INSERT INTO users(
           kakao_id,
           name,
           email,
           birthday,
           phone_number
         ) VALUES (?,?,?,?,?)`,
-        [kakaoId, name, email, birthday, phone_number]
-      );
-    } catch {
-      const error = new Error('dataSource Error');
-      error.statusCode = 400;
-  
-      throw error;
-    }
-  };
+      [kakaoId, name, email, birthday, phone_number]
+    );
+  } catch {
+    const error = new Error("dataSource Error");
+    error.statusCode = 400;
+
+    throw error;
+  }
+};
 
 module.exports = {
-    createAdmin,
-    getUserByEmail,
-    createUser
-}
+  createAdmin,
+  getUserByEmail,
+  createUser,
+};
